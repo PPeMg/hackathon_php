77fa7be0ab2b2e74d863f8779b95a6999b7dd864
PPeMg <ppemg@test.es>
2018-11-24 17:38:05 +0100

    Added Patients List and Insert

--- application/views/patients/dtPatients_List.js
+++ application/views/patients/dtPatients_List.js
@@ -1,2 +1,38 @@
-<script>    
+
+
+
+
+
+
+
+
+<script>
+    const urlGetPatients = "<?=site_url('/Patients/getPatientsList')?>";
+    $.ajax({
+        url: urlGetPatients,
+        type: "POST",
+        data: {}
+    })
+        .done(function (patients) {
+            console.log(patients);
+            if (JSON.parse(patients).length === 0) {
+                $('.noResult').show()
+            } else {
+                $('.waitingResult').hide();
+                $(JSON.parse(patients)).each( function (index, patient) {
+                    var html = `<tr>
+                        <td>${patient.IDENTIFICADOR}</td>
+                        <td>${patient.NOMBRE}</td>
+                        <td>${patient.APELLIDOS}</td>
+                        <td>${patient.DNI}</td>
+                        <td>${patient.TELEFONO}</td>
+                        <td>${patient.EMAIL}</td>
+                    `
+                    $('tbody').append(html)
+                })
+            }
+        })
+        .fail(function (err) {
+            $('.noResult').show()
+        });
 </script>
